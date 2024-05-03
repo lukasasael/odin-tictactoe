bed5436a03af7a0f3b1d6205e9f390b3b9ab736e
Lukas Asael <lukas.asael@werbb.co>
2024-05-03 10:52:42 -0300

    UI implementions
    Linking the logic with the UI

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function gameBoard() {
   function dropMarker(row, column, player) {
     if (row > 2 || column > 2)
       return console.error("Posição Inválida");
-    if (board[row][column].getValue() == 0) {
+    if (board[row][column].getValue() == "-") {
       board[row][column].addMarker(player.marker);
     } else {
       console.log("A célula tá ocupada");
@@ -35,7 +35,7 @@ function gameBoard() {
 }
 
 function Cell() {
-  let value = 0;
+  let value = "-";
 
   const addMarker = (playerMarker) => {
     value = playerMarker;
@@ -50,7 +50,6 @@ function Cell() {
 
 function gameController() {
   const board = gameBoard();
-  displayRender(board.getBoard());
 
   let player = [];
 
@@ -58,11 +57,29 @@ function gameController() {
     return { name, marker };
   }
 
-  for (let i = 0; i < 2; i++) {
-    var name = prompt("Player " + i + " Name:");
-    var mark = prompt("Player " + i + " Mark:");
-    player[i] = createPlayer(name, mark);
-  }
+  const name1 = (document.getElementById("player-1-name")).value;
+  const marker1 = (document.getElementById("player-1-marker")).value;
+  const name2 = (document.getElementById("player-2-name")).value;
+  const marker2 = (document.getElementById("player-2-marker")).value;
+
+  const inputArea = document.getElementById("input-area");
+
+
+  document.getElementById("name1").innerHTML = (document.getElementById("player-1-name")).value;
+  document.getElementById("marker1").innerHTML = "X";
+  document.getElementById("marker1").style.color = marker1;
+  newMarker1 = document.getElementById("marker1").innerHTML;
+
+  document.getElementById("name2").innerHTML = (document.getElementById("player-2-name")).value;
+  document.getElementById("marker2").innerHTML = "O";
+  document.getElementById("marker2").style.color = marker2;
+  newMarker2 = document.getElementById("marker2").innerHTML;
+  //Consertar essa merda aí pra printar colorido no tictactoe-area
+
+  inputArea.remove();
+
+  player[0] = createPlayer(name1, newMarker1);
+  player[1] = createPlayer(name2, newMarker2);
 
   let activePlayer = player[0];
 
@@ -70,44 +87,34 @@ function gameController() {
     activePlayer = activePlayer === player[0] ? player[1] : player[0];
   }
 
-  function playRound() {
-    var row = prompt("que linha quer jogar?");
-    var column = prompt("que coluna quer jogar?");
-    board.dropMarker(row, column, activePlayer);
-  }
+  displayRender(board.getBoard(), playRound);
 
-  let gameGoing = true;
-
-  while (gameGoing) {
+  function playRound(i, j) {
+    board.dropMarker(i, j, activePlayer);
     cellArray = board.getBoard();
     if (threeInRow(cellArray)) {
       gameGoing = false;
       switchActualPlayer();
       console.log("Game Over " + activePlayer.name + " Won!");
       console.log(cellArray);
-      break
     } if (itsATie(cellArray)) {
       gameGoing = false;
       console.log("Game Over Its a Tie!");
       console.log(cellArray);
-      break
-    }
-    else {
-      playRound();
     }
     switchActualPlayer();
   }
 
   function threeInRow(cellArray) {
     for (let i = 0; i < 3; i++) {
-      if ((cellArray[i][0] != 0) && (cellArray[i][0] == cellArray[i][1]) && (cellArray[i][1] == cellArray[i][2]))
+      if ((cellArray[i][0] != "-") && (cellArray[i][0] == cellArray[i][1]) && (cellArray[i][1] == cellArray[i][2]))
         return true
-      if ((cellArray[0][i] != 0) && (cellArray[0][i] == cellArray[1][i]) && (cellArray[1][i] == cellArray[2][i]))
+      if ((cellArray[0][i] != "-") && (cellArray[0][i] == cellArray[1][i]) && (cellArray[1][i] == cellArray[2][i]))
         return true
     }
-    if ((cellArray[1][1] != 0) && (cellArray[0][0] == cellArray[1][1]) && (cellArray[1][1] == cellArray[2][2]))
+    if ((cellArray[1][1] != "-") && (cellArray[0][0] == cellArray[1][1]) && (cellArray[1][1] == cellArray[2][2]))
       return true
-    if ((cellArray[1][1] != 0) && (cellArray[0][2] == cellArray[1][1]) && (cellArray[1][1] == cellArray[2][0]))
+    if ((cellArray[1][1] != "-") && (cellArray[0][2] == cellArray[1][1]) && (cellArray[1][1] == cellArray[2][0]))
       return true
     else
       return false
@@ -117,37 +124,32 @@ function gameController() {
     let testArray = [];
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (cellArray[i][j] != 0)
+        if (cellArray[i][j] != "-")
           testArray.push(1);
       }
     }
-    console.log(testArray)
+    //console.log(testArray)
     if (testArray.length == 9)
       return true
     return false
   }
 }
 
-function displayRender(board) {
+function displayRender(board, playRound) {
   board[0][1] = 'X';
   board[1][1] = 'O';
+  console.log(board);
 
-  const name1 = (document.getElementById("player-1-name")).value;
-  const marker1 = (document.getElementById("player-1-marker")).value;
-  const name2 = (document.getElementById("player-2-name")).value;
-  const marker2 = (document.getElementById("player-2-marker")).value;
+  const tictactoe = document.getElementById("tictactoe-area");
+  const cell = [];
 
-  const inputArea = document.getElementsByClassName("input-area");
-  //deletar inputarea
-  
-  const tictactoe = document.getElementsByClassName("tictactoe-area");
-
-  const node = document.createElement("div");
-  node.classList.add("node");
-  //node.textContent = "Glimpse Of Usssssssssss";
-  //node.innerHTML = `${Cell()}`;
   for (let i = 0; i < 3; i++) {
-
+    for (let j = 0; j < 3; j++) {
+      cell[i + j] = document.createElement("div");
+      cell[i + j].classList.add("cell");
+      cell[i + j].innerHTML = board[i][j];
+      cell[i + j].onclick = playRound(i, j, activePlayer);
+      tictactoe.appendChild(cell[i + j]);
+    }
   }
-  tictactoe.appendChild(node);
 }
